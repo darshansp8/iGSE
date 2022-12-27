cb362baf646797ac696baba7e785044ec2427c9c
Darshan Sakpal <darshansakpal@pc-196-4.customer.ask4.lan>
2022-12-27 00:27:08 +0000

    Added Registration and Login Page

--- iGSE-UI/src/app/app.module.ts
+++ iGSE-UI/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './Users/users/users.component';
-import { UserAuthenticationComponent } from './Users/user-authentication/user-authentication.component';
 import { UserMeterReadingComponent } from './Users/user-meter-reading/user-meter-reading.component';
 import { UserBillComponent } from './Users/user-bill/user-bill.component';
 import { UserTopupComponent } from './Users/user-topup/user-topup.component';
@@ -12,19 +11,23 @@ import { AdminComponent } from './Admin/admin/admin.component';
 import { AdminMeterReadingComponent } from './Admin/admin-meter-reading/admin-meter-reading.component';
 import { AdminStatisticsComponent } from './Admin/admin-statistics/admin-statistics.component';
 import { AdminSetPriceComponent } from './Admin/admin-set-price/admin-set-price.component';
+import { LoginComponent } from './Authentication/login/login.component';
+import { RegisterComponent } from './Authentication/register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserAuthenticationComponent,
     UserMeterReadingComponent,
     UserBillComponent,
     UserTopupComponent,
     AdminComponent,
     AdminMeterReadingComponent,
     AdminStatisticsComponent,
-    AdminSetPriceComponent
+    AdminSetPriceComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
